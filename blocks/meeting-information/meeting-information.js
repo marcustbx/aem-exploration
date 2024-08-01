69728c77079c4b32fe2ce35fc136c727f5eeaee6
marcustbx <marcus@thinkingbox.com>
2024-08-01 14:27:49 -0400

    [#0] - New block and new global style

---
+++ blocks/meeting-information/meeting-information.js
@@ -0,0 +1,8 @@
+export default function decorate(block) { 
+    const links = block.querySelectorAll('a'); 
+    links.forEach(link => { link.classList.add('meeting-link'); }); 
+    const buttons = block.querySelectorAll('button'); 
+    buttons.forEach(button => { button.classList.add('response-button'); }); 
+}
+
+console.log('test');
