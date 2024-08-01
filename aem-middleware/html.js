70df9a535b98bf383a2e359138efc0beec9fa4ca
marcustbx <marcus@thinkingbox.com>
2024-08-01 14:10:12 -0400

    [#0] - New creds, gitignore

--- aem-middleware/html.js
+++ aem-middleware/html.js
@@ -1,83 +1,132 @@
-const htmlString = `<!DOCTYPE html> 
-<html lang="en"> 
-    <head>  
-        <meta charset="UTF-8"> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">  
-        <title>Internal Project Initiative</title> 
-        <link rel="stylesheet" href="styles.css"> 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> 
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"> 
-    </head>  
-    <body> 
-        <hr class="pb"> 
-        <table>
-            <thead>
-                <tr> 
-                    <th colspan="2">Section Metadata</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Style</td>
-                    <td>main-section</td>
-                </tr>
-            </tbody>
-        </table>
-        <table>
-            <thead>
-                <tr>
-                    <th colspan="2">Card Block</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <h1>INT: Internal Project Initiative</h1>  
-                        <p>Wednesday, July 31 • 5:00 - 5:30pm</p>
-                        <a href="#" class="join-button">Join with Google Meet</a>
-                        <p class="google-meet-link">meet.google.com/kcp-hijc-jep</p>
-                        <p> 
-                            <a href="#" class="phone-link">
-                                <i class="fas fa-phone-alt"></i>
-                                Join by phone
-                            </a>
-                        </p>
-                        <p class="phone-number">(CA) +1 289-351-9533 PIN: 748 674 352#</p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <table>
-            <thead>
-                <tr>
-                    <th colspan="2">Details Block</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <p><a href="#" class="more-phone-link">More phone numbers</a></p>
-                        <div class="guests">
-                            <h2>3 guests</h2>
-                            <p>1 yes • 1 no • 1 awaiting</p>
-                            <ul class="guest-list">
-                                <li>Satoru Inoue <span class="organizer">Organizer</span></li>
-                                <li>  Justin Desjardins  <span class="status">  <i class="fas fa-plane"></i> Declined because I am on vacation  </span>  </li>
-                                <li>  Patrick Daggitt  <span class="status">  <i class="fas fa-briefcase"></i> Office • <a href="#">Edit</a>  </span>  </li>
-                            </ul>
-                        </div>
-                        <p>10 minutes before <strong>Satoru Inoue</strong></p>
-                        <div class="response-buttons">
-                            <button class="response-yes">Yes</button>
-                            <button class="response-no">No</button>
-                            <button class="response-maybe">Maybe</button>
-                        </div>
-                    </td>
-                </tr>
-            </tbody>
-        </table>  
-        <hr class="pb">  
-    </body>
+const htmlString = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Internal Project Initiative</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <hr class="pb">
+    <table>
+        <thead>
+            <tr>
+                <th colspan="2">Section Metadata</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Style</td>
+                <td>meeting-section</td>
+            </tr>
+        </tbody>
+    </table>
+    <table>
+        <thead>
+            <tr>
+                <th colspan="2">Meeting Information</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>INT: Internal Project Initiative</td>
+            </tr>
+            <tr>
+                <td>Wednesday, July 31 5:00 – 5:30pm</td>
+            </tr>
+            <tr>
+                <td><a href="https://meet.google.com/kcp-hijc-jep">Join with Google Meet</a></td>
+            </tr>
+            <tr>
+                <td>Join by phone <table>
+                        <thead>
+                            <tr>
+                                <th colspan="2">Phone Info</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>(CA) +1 289-351-9533 PIN: 748 674 352#</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>More phone numbers</td>
+            </tr>
+            <tr>
+                <td>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th colspan="2">Guests</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>3 guests</td>
+                            </tr>
+                            <tr>
+                                <td>1 yes</td>
+                            </tr>
+                            <tr>
+                                <td>1 no</td>
+                            </tr>
+                            <tr>
+                                <td>1 awaiting</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table>
+                                        <thead>
+                                            <tr>
+                                                <th colspan="2">Guest Info</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Satoru Inoue</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Organizer</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Justin Desjardins</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Declined because I am on vacation</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Patrick Daggitt</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Office <a href="#">Edit</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>10 minutes before</td>
+            </tr>
+            <tr>
+                <td>Satoru Inoue</td>
+            </tr>
+            <tr>
+                <td>Going? <button>Yes</button> <button>No</button> <button>Maybe</button> </td>
+            </tr>
+        </tbody>
+    </table>
+    <hr class="pb">
+</body>
+
 </html>`;
 
 export default htmlString;
--- aem-middleware/main.js
+++ aem-middleware/main.js
@@ -1,57 +1,63 @@
-import http from 'http';
+import http from "http";
 import { google } from 'googleapis';
 import htmlString from './html.js';
 
 const googleAuth = new google.auth.GoogleAuth({
-  keyFilename: './etc/keys/aem-middleware-80c27348d0de.json',
-  scopes: ['https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive'],
+  keyFilename: './etc/keys/aem-middleware-40e1668c153a.json',
+  scopes: [ 'https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive' ]
 });
 
 const host = 'localhost';
 const port = 8000;
 const docs = google.docs({ version: 'v1', auth: googleAuth });
-const drive = google.drive({ version: 'v3', auth: googleAuth });
+const drive = google.drive({ version: 'v3', auth: googleAuth });   
 const folderId = '1_ar20YK9tr-bdtSLKYZrblAMNrV4U12J';
 const fileName = 'test';
 const fileMetadata = { name: fileName, parents: [folderId], mimeType: 'application/vnd.google-apps.document' };
 const html = { mimeType: 'text/html', body: htmlString };
 
-const findFile = async () => {
-  const res = await drive.files.list({
-    q: `name='${fileName}' and trashed=false`,
-    fields: 'files(id, name)',
-  });
+const findFile = async drive => {
 
-  return res.data.files;
-};
-
-const deleteFile = async (fileId) => { await drive.files.delete({ fileId }); };
-
-const requestListener = async (req, res) => {
-  const createResponse = await docs.documents.create();
-  const { documentId } = createResponse.data;
-
-  try {
-    const files = await findFile(drive, fileName);
-
-    if (files.length > 0) {
-      await deleteFile(drive, files[0].id);
-      console.log(`Deleted existing file with ID: ${files[0].id}`);
-    }
-
-    drive.files.create({
-      requestBody: fileMetadata,
-      media: html,
-      fields: 'id',
+    const res = await drive.files.list({
+        q: `name='${fileName}' and trashed=false`,
+        fields: 'files(id, name)',
     });
 
-    console.log(`File uploaded with ID: ${documentId}`);
+    return res.data.files;
+}
+  
+const deleteFile = async (drive, fileId) => { await drive.files.delete({ fileId }); }
 
-  } catch (error) { console.error('Error uploading file:', error); }
+const requestListener = async (req, res) => {
 
-  res.writeHead(200);
-  res.end('AEM Middleware has worked succesfully!');
+    const createResponse = await docs.documents.create();
+    const documentId = createResponse.data.documentId;
+    
+    try {
+
+        const files = await findFile(drive, fileName);
+
+        if ( files.length > 0 ) {
+
+            await deleteFile(drive, files[0].id);
+            console.log(`Deleted existing file with ID: ${files[0].id}`);
+        }
+
+        drive.files.create({
+            requestBody: fileMetadata,
+            media: html,
+            fields: 'id'
+        });
+
+        console.log(`File uploaded with ID: ${documentId}`);
+
+    } catch (error) { console.error('Error uploading file:', error); }
+
+    res.writeHead(200);
+    res.end("AEM Middleware has worked succesfully!");
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => { console.log(`Server is running on http://${host}:${port}`); });
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
