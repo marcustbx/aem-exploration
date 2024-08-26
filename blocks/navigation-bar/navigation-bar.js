4e4cd1bbc6416919c3bfe55d9c5bc003a885a2a9
Marcus Harding <159040624+marcustbx@users.noreply.github.com>
2024-08-26 15:35:45 -0400

    Delete file via API

--- blocks/navigation-bar/navigation-bar.js
+++
@@ -1,8 +0,0 @@
-export default function decorate(block) {
-  const ulTag = block.querySelector('ul');
-  ulTag.classList.add('nav-list');
-  const aTags = block.querySelectorAll('a');
-  aTags.forEach((a) => {
-    a.classList.add('nav-link');
-  });
-}
