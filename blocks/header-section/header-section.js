6acd45c63985576987572a2d27bf56820c67c7fe
Marcus Harding <159040624+marcustbx@users.noreply.github.com>
2024-08-26 15:26:27 -0400

    Delete file via API

--- blocks/header-section/header-section.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  const h1Tag = block.querySelector('h1');
-  h1Tag.classList.add('header-title');
-  const pTag = block.querySelector('p');
-  pTag.classList.add('header-description');
-}
