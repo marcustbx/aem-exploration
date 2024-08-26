d6b8dd73eb4918392d6210e542cca69b2a320f75
Marcus Harding <159040624+marcustbx@users.noreply.github.com>
2024-08-26 15:26:30 -0400

    Delete file via API

--- blocks/featured-section/featured-section.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block) {
-  const h2Tag = block.querySelector('h2');
-  h2Tag.classList.add('featured-title');
-  const pTag = block.querySelector('p');
-  pTag.classList.add('featured-description');
-}
