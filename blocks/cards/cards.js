de7d7663c886a2d496f3e2c98b4b2aadd1b779ea
Marcus Harding <159040624+marcustbx@users.noreply.github.com>
2024-11-14 11:22:48 -0500

    Delete file via API

--- blocks/cards/cards.js
+++
@@ -1,9 +0,0 @@
-export default function decorate(block) {
-  const cards = block.querySelectorAll('.card');
-  cards.forEach((card) => {
-    const overlay = card.querySelector('.overlay');
-    overlay.classList.add('overlay-content');
-    const button = overlay.querySelector('.button');
-    button.classList.add('overlay-button');
-  });
-}
